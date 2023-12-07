10abb3473ea810ffd4ee5054d12f1c8f308105e1
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 18:16:16 +0530

    Added Databse and Storage onto firebase

--- share-box/firebase.ts
+++ share-box/firebase.ts
@@ -1,4 +1,6 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA-l-9ACEaLLkP-gB2Y_0KZBV1To4gbu4o",
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
   const app = getApps().length? getApp():initializeApp(firebaseConfig) //if already initlaised a value is present , tehn we get an app ,lese we need to intlaise 
   //we initlaise only once
+  const db = getFirestore(app)
+  const storage = getStorage(app)
