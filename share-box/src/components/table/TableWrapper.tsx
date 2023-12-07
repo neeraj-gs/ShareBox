13ac153f844e07dd49b4fa1f5dc21320750aadd4
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 19:50:24 +0530

    Added ssr pag ehtat gets the files uploaded

--- share-box/src/components/table/TableWrapper.tsx
+++ share-box/src/components/table/TableWrapper.tsx
@@ -1,10 +1,14 @@
 import { FileType } from '@/typings'
 import React from 'react'
 import { Button } from '../ui/button'
+import { DataTable } from './Table'
+import { columns } from './Columns'
 
 const TableWrapper = ({skeletonFiles}:{skeletonFiles:FileType[]}) => {
   return (
     <div>
+        <Button>Sort By ...</Button>
+        <DataTable columns={columns} data={skeletonFiles} />
 
     </div>
   )
