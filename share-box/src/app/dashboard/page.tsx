fa11ddea67d63f5dd54cfbe45c6f24c5739ecdb2
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 17:50:59 +0530

    Added A Drag and Drop feature into the dashboard

--- share-box/src/app/dashboard/page.tsx
+++ share-box/src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
-import { auth } from "@clerk/nextjs"
-import Dropzone from "react-dropzone";
+import DropZone from "@/components/DropZone";
+import { auth } from "@clerk/nextjs";
 
 const DashBoardpage = () => {
     const {userId} = auth();
   return (
     <main>
-        <Dropzone />
+        <DropZone />
     </main>
   )
 }
--- share-box/src/components/DropZone.tsx
+++ share-box/src/components/DropZone.tsx
@@ -2,13 +2,31 @@
 
 import Dropzone from 'react-dropzone'
 
-<Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-  {({getRootProps, getInputProps}) => (
+const DropZone = () => {
+    const maxsize = 20971520;
+
+  return (
+    <div>
+        <Dropzone minSize={0} maxSize={maxsize} onDrop={acceptedFiles => console.log(acceptedFiles)}>
+  {({getRootProps, getInputProps,isDragActive,isDragReject,fileRejections,}) => {
+
+    const isFileTooLarge = fileRejections.length >0 && fileRejections[0].file.size > maxsize
+
+    return(
     <section>
       <div {...getRootProps()}>
         <input {...getInputProps()} />
-        <p>Drag and drop some files here, or click to select files</p>
+        {!isDragActive && "Click Here or Drop a File to be Uploaded"}
+        {!isDragActive && !isDragReject && "Drop Here to Upload the File"}
+        {isFileTooLarge && (
+            <div className='text-danger mt-3'>File Exceeds the Storage Limit , Reduce the Size</div>
+        )}
       </div>
     </section>
-  )}
+  )}}
 </Dropzone>
+    </div>
+  )
+}
+
+export default DropZone
