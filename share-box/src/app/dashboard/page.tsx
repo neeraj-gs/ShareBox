fd8fbba354b973be7e04b1fc7402a3b1ab3b21d0
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 19:10:06 +0530

    Added code to render the files server side and give the data onto the frontend on initial render

--- share-box/src/app/dashboard/page.tsx
+++ share-box/src/app/dashboard/page.tsx
@@ -1,8 +1,29 @@
 import DropZone from "@/components/DropZone";
+import { db } from "@/firebase";
+import { FileType } from "@/typings";
 import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 
-const DashBoardpage = () => {
+const DashBoardpage = async() => {
     const {userId} = auth();
+
+    //server side rendered , pre load the docuements presetn on the server
+    const docResults = await getDocs(collection(db,"users",userId!,"files"))
+    const skeletonFiles:FileType[] = docResults.docs.map(doc => ({
+      //object thaqt will bubble up adn return an array -- implicit Return
+      id:doc.id,
+      filename:doc.data().filename || doc.id,
+      timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+      fulName:doc.data().fullName,
+      downloadUrl:doc.data().downloadUrl,
+      type: doc.data().type,
+      size: doc.data().size,
+      
+    }))
+
+    
+
+
   return (
     <main>
         <DropZone />
---
+++ share-box/src/typings.ts
@@ -0,0 +1,9 @@
+export type FileType = {
+    id:string;
+    filename:string
+    fulName:string;
+    timestamp:Date;
+    downloadUrl:string;
+    type:string;
+    size:number;
+}
