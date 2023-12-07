d94f2ff6c748ae4ab6003ee756da99ae05e73aa8
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 19:54:53 +0530

    Added a sorting functionality to add the top or old files into the pae

--- share-box/src/components/table/TableWrapper.tsx
+++ share-box/src/components/table/TableWrapper.tsx
@@ -1,15 +1,23 @@
+"use client"
+
 import { FileType } from '@/typings'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../ui/button'
 import { DataTable } from './Table'
 import { columns } from './Columns'
+import { useUser } from '@clerk/nextjs'
 
 const TableWrapper = ({skeletonFiles}:{skeletonFiles:FileType[]}) => {
+    const {user} = useUser();
+    const {initialiFiles,setInitialiFiles} = useState<FileType[]>([]);
+    const [sort,setSort] = useState<"asc"|"desc">("desc")
+
   return (
     <div>
-        <Button>Sort By ...</Button>
+        <Button
+        onClick={()=> setSort(sort === "desc" ? "asc" : "desc")}
+        >Sort By {sort === "desc"? "Newest First": "Oldest First"}</Button>
         <DataTable columns={columns} data={skeletonFiles} />
-
     </div>
   )
 }
