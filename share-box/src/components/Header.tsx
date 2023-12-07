d8bafbccbe50d9a8041369025526df6946443d01
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 11:16:58 +0530

    Added Logo image and rendered on page

--- share-box/src/components/Header.tsx
+++ share-box/src/components/Header.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <header>
-        
+        <nav className="flex items-center justify-between">
+            <Link href="/">
+                <div>
+                    <Image src="/dropbox.png" alt="logo"  height={50} width={50}/>
+                </div>
+                <h1 className="font-bold text-xl">ShareBox</h1>
+            </Link>
+            <div></div>
+        </nav>
     </header>
   )
 }
