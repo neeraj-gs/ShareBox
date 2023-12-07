aa56ab4c50ac57ed464415cf3abf686a54451617
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 11:53:36 +0530

    Added SOme basic styles and also implemetned teh main section and added code

--- share-box/src/app/page.tsx
+++ share-box/src/app/page.tsx
@@ -1,10 +1,29 @@
-import Image from 'next/image'
-import { UserButton } from "@clerk/nextjs";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      
+      <div>
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">Welcome to ShareBox.
+          <br />
+          <br />
+          Storing Everything you and Your Business Needs. All in One Place Store for collaboration.
+          </h1>
+
+          <p>Ehance your Personilzed storage ,offering a simple 
+            and 
+            efficient way to upload,organize and access files from anywhere.
+            Securely share with people and collaborate with your peers, and experience easy file managemnet.
+          </p>
+
+          <Link href='/dashboard' className="flex gap-3 p-4 hover:cursor-pointer bg-slate-300 w-fit text-black" >Get Started <ArrowRight className="ml-4" /> </Link>
+          
+          
+           
+        </div>
+      </div>
     </main>
   )
 }
