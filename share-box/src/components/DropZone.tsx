1127eae3ee090ed734fc984cb986405e824d2c94
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 17:57:32 +0530

    Styled the Drag and drop box and allowed user to upload or drag and drop

--- share-box/src/components/DropZone.tsx
+++ share-box/src/components/DropZone.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { cn } from '@/lib/utils';
 import Dropzone from 'react-dropzone'
 
 const DropZone = () => {
@@ -13,11 +14,16 @@ const DropZone = () => {
     const isFileTooLarge = fileRejections.length >0 && fileRejections[0].file.size > maxsize
 
     return(
-    <section>
-      <div {...getRootProps()}>
+    <section className='m-4'>
+      <div {...getRootProps()}
+        className={cn("w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg text-center"
+        ,isDragActive ? "bg-[#035ffe] text-white animate-pulse":"bg-slate-100/50 dark:bg-slate-800/80 text-slate-400 dark:text-white"
+        )}
+      >
+
         <input {...getInputProps()} />
         {!isDragActive && "Click Here or Drop a File to be Uploaded"}
-        {!isDragActive && !isDragReject && "Drop Here to Upload the File"}
+        {isDragActive && !isDragReject && "Drop Here to Upload the File"}
         {isFileTooLarge && (
             <div className='text-danger mt-3'>File Exceeds the Storage Limit , Reduce the Size</div>
         )}
