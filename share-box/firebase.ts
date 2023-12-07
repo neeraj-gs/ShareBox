8e5bd37928919503a60f3550dba9a65333d9f7a5
neeraj-gs <gsneeraj05@gmail.com>
2023-12-07 18:15:21 +0530

    Added firebase onfig and need to add more configs to dba dn auth

---
+++ share-box/firebase.ts
@@ -0,0 +1,14 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-l-9ACEaLLkP-gB2Y_0KZBV1To4gbu4o",
+    authDomain: "sharebox-3c4d2.firebaseapp.com",
+    projectId: "sharebox-3c4d2",
+    storageBucket: "sharebox-3c4d2.appspot.com",
+    messagingSenderId: "914120536614",
+    appId: "1:914120536614:web:c2d510ba9e8ebbaeaa2ce7"
+  };
+
+
+  const app = getApps().length? getApp():initializeApp(firebaseConfig) //if already initlaised a value is present , tehn we get an app ,lese we need to intlaise 
+  //we initlaise only once
